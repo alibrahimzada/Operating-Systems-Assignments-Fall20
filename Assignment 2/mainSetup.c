f3ae77fbc7666f814fd9feb5f9c6eee1d87d178d
alibrahimzada <alibrahimzada@gmail.com>
2020-12-14 22:25:47 +0300

    added part A

--- Assignment 2/mainSetup.c
+++ Assignment 2/mainSetup.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
  
 #define MAX_LINE 80 /* 80 chars per line, per command, should be enough. */
  
@@ -9,8 +10,7 @@ in the next command line; separate it into distinct arguments (using blanks as
 delimiters), and set the args array entries to point to the beginning of what
 will become null-terminated, C-style strings. */
 
-void setup(char inputBuffer[], char *args[],int *background)
-{
+void setup(char inputBuffer[], char *args[], int *background) {
     int length, /* # of characters in the command line */
         i,      /* loop index for accessing inputBuffer array */
         start,  /* index where beginning of next command parameter is */
@@ -19,7 +19,7 @@ void setup(char inputBuffer[], char *args[],int *background)
     ct = 0;
         
     /* read what the user enters on the command line */
-    length = read(STDIN_FILENO,inputBuffer,MAX_LINE);  
+    length = read(STDIN_FILENO, inputBuffer, MAX_LINE);  
 
     /* 0 is the system predefined file descriptor for stdin (standard input),
        which is the user's screen in this case. inputBuffer by itself is the
@@ -31,68 +31,92 @@ void setup(char inputBuffer[], char *args[],int *background)
     if (length == 0)
         exit(0);            /* ^d was entered, end of user command stream */
 
-/* the signal interrupted the read system call */
-/* if the process is in the read() system call, read returns -1
-  However, if this occurs, errno is set to EINTR. We can check this  value
-  and disregard the -1 value */
+    /* the signal interrupted the read system call */
+    /* if the process is in the read() system call, read returns -1
+       However, if this occurs, errno is set to EINTR. We can check this  value
+       and disregard the -1 value */
+
     if ( (length < 0) && (errno != EINTR) ) {
         perror("error reading the command");
-	exit(-1);           /* terminate with error code of -1 */
+	    exit(-1);           /* terminate with error code of -1 */
     }
 
 	printf(">>%s<<",inputBuffer);
-    for (i=0;i<length;i++){ /* examine every character in the inputBuffer */
+    for (i = 0; i < length; i++) { /* examine every character in the inputBuffer */
 
-        switch (inputBuffer[i]){
-	    case ' ':
-	    case '\t' :               /* argument separators */
-		if(start != -1){
+        switch (inputBuffer[i]) {
+    	    case ' ':
+	        case '\t' :               /* argument separators */
+		        if(start != -1){
                     args[ct] = &inputBuffer[start];    /* set up pointer */
-		    ct++;
-		}
+		            ct++;
+		        }
                 inputBuffer[i] = '\0'; /* add a null char; make a C string */
-		start = -1;
-		break;
+                start = -1;
+                break;
 
             case '\n':                 /* should be the final char examined */
-		if (start != -1){
+        		if (start != -1){
                     args[ct] = &inputBuffer[start];     
-		    ct++;
-		}
+        		    ct++;
+		        }
                 inputBuffer[i] = '\0';
                 args[ct] = NULL; /* no more arguments to this command */
-		break;
+        		break;
 
-	    default :             /* some other character */
-		if (start == -1)
-		    start = i;
+	        default :             /* some other character */
+        		if (start == -1)
+		            start = i;
                 if (inputBuffer[i] == '&'){
-		    *background  = 1;
+		            *background  = 1;
                     inputBuffer[i-1] = '\0';
-		}
-	} /* end of switch */
-     }    /* end of for */
-     args[ct] = NULL; /* just in case the input line was > 80 */
+		        }
+	    } /* end of switch */
+    }    /* end of for */
+    args[ct] = NULL; /* just in case the input line was > 80 */
 
 	for (i = 0; i <= ct; i++)
 		printf("args %d = %s\n",i,args[i]);
 } /* end of setup routine */
- 
-int main(void)
-{
-            char inputBuffer[MAX_LINE]; /*buffer to hold command entered */
-            int background; /* equals 1 if a command is followed by '&' */
-            char *args[MAX_LINE/2 + 1]; /*command line arguments */
-            while (1){
-                        background = 0;
-                        printf("myshell: ");
-                        /*setup() calls exit() when Control-D is entered */
-                        setup(inputBuffer, args, &background);
-                       
-                        /** the steps are:
-                        (1) fork a child process using fork()
-                        (2) the child process will invoke execv()
-						(3) if background == 0, the parent will wait,
-                        otherwise it will invoke the setup() function again. */
+
+int main(void) {
+    char inputBuffer[MAX_LINE]; /*buffer to hold command entered */
+    int background; /* equals 1 if a command is followed by '&' */
+    char *args[MAX_LINE/2 + 1]; /*command line arguments */
+    while (1) {
+        background = 0;
+        printf("myshell: ");
+        /*setup() calls exit() when Control-D is entered */
+        setup(inputBuffer, args, &background);
+        
+        /** the steps are:
+        (1) fork a child process using fork()
+        (2) the child process will invoke execv()
+        (3) if background == 0, the parent will wait,
+        otherwise it will invoke the setup() function again. */
+
+        pid_t childpid;
+
+        if ( (childpid = fork()) == -1 ) {
+            perror("failed to fork!");
+            continue;
+        }
+        
+        if ( childpid == 0 ) {
+            const char* s = getenv("PATH");
+            if (s == NULL) {
+                perror("getenv returned NULL");
+                continue;
             }
+            printf("%s", s);
+
+            execv("/bin/ls", args);   // replace /bin/command with the program from $PATH
+            perror("child failed when running execv!");
+            continue;
+        }
+
+        if ( !background && wait(NULL) != childpid ) {   // needs some editing
+            perror("A signal must have interrupted the wait!");
+        }
+    }
 }
