21c4e7b63c96677d81c5028675eff0bbe9a368d0
alibrahimzada <alibrahimzada@gmail.com>
2020-12-30 14:58:34 +0300

    organized code

--- Assignment 2/mainSetup.c
+++ Assignment 2/mainSetup.c
@@ -455,7 +455,6 @@ int main(void) {
             pid_t childpid;
 			int redirectionMode = 0;
 			char inputFile[MAX_LINE/2+1], outputFile[MAX_LINE/2+1];
-			strcpy(outputFile, "");
 			int fd;
 			int fd2;
 
@@ -633,6 +632,7 @@ int main(void) {
 
 					// <case 6 : pipe (|)>
 					case 6:
+						strcpy(outputFile, "");
 						do {
 							strcat(outputFile, args[caseLoopVar]);
 							if (args[caseLoopVar + 1] == NULL)
